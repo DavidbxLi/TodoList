5ab7bee36f27484b093c537d9f716880e192f96e
DavidbxLi <liboxi_work@163.com>
2023-04-12 09:23:31 +0800

    feat: 增加React v18适配

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,15 @@
-console.log(213);
-window.navigator;
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app';
 
-export {};
+const container = document.getElementById('root') as Element;
+
+// Create a root.
+const root = createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
